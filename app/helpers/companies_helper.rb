95f18dcc4bf893ffddda9c5830378460a80be70b
Thiago-Gomes <thiagogomes@seduc.ro.gov.br>
2024-07-08 08:18:40 -0400

    ajuste do bug de Criar empresa

--- app/helpers/companies_helper.rb
+++ app/helpers/companies_helper.rb
@@ -1,6 +1,6 @@
 module CompaniesHelper
     def dynamic_url
-        if current_user.company.present?
+        if current_user.company.present? && current_user.company.try(:id).present?
             edit_company_path(current_user.company)
         else
             new_company_path
