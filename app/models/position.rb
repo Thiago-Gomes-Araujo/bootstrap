d066f6f0025a27cc497270510cabd654f9fc1d66
Thiago-Gomes <thiagogomes@seduc.ro.gov.br>
2024-07-11 13:19:18 -0400

    contar caraceter pendente

--- app/models/position.rb
+++ app/models/position.rb
@@ -1,11 +1,13 @@
 class Position < ApplicationRecord
-    belongs_to :company
-    belongs_to :career
-    belongs_to :contract
-    belongs_to :type_vacancy
+  belongs_to :company
+  belongs_to :career
+  belongs_to :contract
+  belongs_to :type_vacancy
+  has_rich_text :description
 
-    validates :career, presence: true
-    validates :contract, presence: true
-    validates :type_vacancy, presence: true
+  validates :career, presence: true
+  validates :contract, presence: true
+  validates :type_vacancy, presence: true
+  validates :summary, length: { maximum: 100 }
 
 end
