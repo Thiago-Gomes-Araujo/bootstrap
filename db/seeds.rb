96f3d9b83c992bbe1660790cb0d2108f40954fef
Thiago-Gomes-Araujo <thiagogomes543@hotmail.com>
2024-07-15 17:10:47 -0400

    Criando dados fakes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,64 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'faker'
+
+# Exemplo para limpar os dados existentes (opcional)
+Position.delete_all
+Company.delete_all
+Career.delete_all
+Contract.delete_all
+State.delete_all
+TypeVacancy.delete_all
+
+# Criar empresas fictícias
+1.times do
+  company = Company.create!(
+    name: Faker::Company.name,
+    url: Faker::Internet.url,
+    user_id: User.first.id # Ajuste isso conforme necessário
+  )
+
+  # Criar carreiras fictícias
+  careers = 5.times.map do
+    Career.create!(
+      name: Faker::Job.field,
+      description: Faker::Lorem.sentence
+    )
+  end
+
+  # Criar contratos específicos
+  contracts = [
+    { name: 'CLT', description: 'Contrato CLT' },
+    { name: 'PJ', description: 'Contrato PJ' },
+    { name: 'CONTRATO', description: 'Contrato Temporário' }
+  ].map do |contract_attributes|
+    Contract.create!(contract_attributes)
+  end
+  
+  # Criar tipos de vagas fictícias
+  type_vacancies = 15.times.map do
+    TypeVacancy.create!(
+      name: Faker::Job.position
+    )
+  end
+
+  # Criar estados fictícios
+  states = 27.times.map do
+    State.create!(
+      name: Faker::Address.state
+    )
+  end
+
+  # Criar posições fictícias
+  250.times do
+    Position.create!(
+      name: Faker::Job.title,
+      city: Faker::Address.city,
+      summary: Faker::Lorem.sentence,
+      publish: Faker::Boolean.boolean,
+      company_id: company.id,
+      career_id: careers.sample.id,
+      contract_id: contracts.sample.id,
+      type_vacancy_id: type_vacancies.sample.id,
+      state_id: states.sample.id
+    )
+  end
+end
